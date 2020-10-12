295e0c32f82c34d461fe3c8c02b7900d3c464e99
GinmerChavez <46727990+GinmerChavez@users.noreply.github.com>
2020-10-12 02:22:20 -0600

    Update Programaprueba.cpp

--- Programaprueba/Programaprueba/Programaprueba.cpp
+++ Programaprueba/Programaprueba/Programaprueba.cpp
@@ -15,6 +15,7 @@ int main()
     std::cout << "Crack";
     std::cout << "Pero que ha pasao!?";
     //code to call feature 1//code to callfeature 2
+    //addded code to callfeature 2
     system("pause");
 }
 
