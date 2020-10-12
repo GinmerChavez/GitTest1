416c895e7daa5e2151db6110c8f5881044557cb0
Ginmer Chavez <GinmerChavez@outlook.es>
2020-10-12 02:29:19 -0600

    aja

--- Programaprueba/Programaprueba/Programaprueba.cpp
+++ Programaprueba/Programaprueba/Programaprueba.cpp
@@ -15,7 +15,7 @@ int main()
     std::cout << "Crack";
     std::cout << "Pero que ha pasao!?";
     //code to call feature 1//code to callfeature 2
-    //addded code to callfeature 2
+    //addded code to callfeature 3
     system("pause");
 }
 
